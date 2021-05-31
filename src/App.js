bb392895ddfcc6e76bf0663bf1c5f91d650693af
ajaykumar <majaykumar481@gmail.com>
2021-05-31 20:18:52 +0530

    v2 updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import { useState } from "react";
 
 function App() {
   const [list] = useState([1, 2, 3, 4, 5, 6]);
-  return <div>Mini Facebook</div>;
+  return (
+    <div class="text-center">
+      <h1 className="bg-secondary sticky-top" style={{ height: "60px" }}>
+        Mini facebook
+      </h1>
+      {list.map((index, item) => (
+        <div key={index}>
+          <img
+            src={"https://picsum.photos/300" + index}
+            alt=""
+            className="bg-cover m-2 rounded"
+            style={{ height: "200px", width: "100vw" }}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 export default App;
